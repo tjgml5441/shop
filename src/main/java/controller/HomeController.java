4550ea085c5737c5477465e71a3581f2f3895f2f
GD <GD@DESKTOP-RNE9D1F>
2025-11-03 10:30:51 +0900

    커밋 연습2 - home.jsp 포워딩 구현

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -7,19 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class HomeController
- */
-@WebServlet("/HomeController")
+@WebServlet("/home")
 public class HomeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);)
 	}
-
 }
